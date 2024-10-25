30d01f82549b6caaa459158ca0d28bbefebf3717
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-10-24 23:05:03 -0300

    fix: mudar verbose_name para Company

--- apps/situations/models.py
+++ apps/situations/models.py
@@ -3,7 +3,7 @@ from apps.companies.models import Company
 
 
 class Situation(models.Model):
-    company = models.ForeignKey(Company, verbose_name="Category", on_delete=models.PROTECT) 
+    company = models.ForeignKey(Company, verbose_name="Company", on_delete=models.PROTECT) 
     description = models.TextField(null=True, blank=True) 
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
