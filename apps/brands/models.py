5c5b1d7b434858a740a35fe23486fcb0fbbb418c
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-10-24 22:57:11 -0300

    feat: criar app brands

---
+++ apps/brands/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from apps.companies.models import Company
+
+
+class Brand(models.Model):
+    company = models.ForeignKey(Company, verbose_name="Category", on_delete=models.PROTECT) 
+    description = models.TextField(null=True, blank=True) 
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
