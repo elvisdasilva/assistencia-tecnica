9b57435589dce6cb3bed822ab875a69d7d2ed1c7
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-10-29 21:35:20 -0300

    feat: adicionar company

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from apps.companies.models import Company
 
 
 class User(AbstractUser):
-    pass
+    company = models.ForeignKey(Company, verbose_name="Company", on_delete=models.PROTECT, null=True, blank=True)
