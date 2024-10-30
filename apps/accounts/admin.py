d1d48a06caf8e1d8794efa7b0c6c365676a05b6f
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-10-29 21:38:10 -0300

    refactor: atualizar save_model

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -5,11 +5,10 @@ from apps.accounts.models import User
 
 class UserAdmin(admin.ModelAdmin):
     filter_horizontal = ("groups", "user_permissions")
-    
+
     def save_model(self, request, obj, form, change):
         if not change:
             obj.set_unusable_password()
-            obj.is_active = False
             obj.save()
             SendResetPasswordEmailView().send_email(obj)
 
