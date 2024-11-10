0ff92c99dcd5a35504fcb7bddabf9cb65890deb6
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-11-09 22:47:41 -0300

    feat: criar método static

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.urls import reverse
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.views import View
+
 from core.settings import EMAIL_HOST_USER
-from django.contrib.auth.models import User
 
 
 class SendResetPasswordEmailView(View):
-    def send_email(self, user):
+    @staticmethod
+    def send_email(user):
         token_generator = PasswordResetTokenGenerator()
         token = token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
